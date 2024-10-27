c23dee99fa208c52306efc459ff2eec6a0481432
nr24wcs <npr316@hotmail.com>
2024-10-27 12:53:25 -0400

    Add files via upload

---
+++ main calculator mini project.py
@@ -0,0 +1,10 @@
+first_number = float(input("First Number: " ))
+second_number = float(input("Second Number: "))
+sum = first_number + second_number
+subtraction = first_number - second_number
+multiplication= first_number * second_number
+division = first_number / second_number
+print("Sum is:", sum)
+print("Subtraction is:", subtraction)
+print("Multiplication is:", multiplication)
+print("Division is:", division)
